fe6f0eee9e22652a6378595e490760b37971068e
makspll <makspl17@gmail.com>
2021-12-14 02:13:26 +0000

    change math.h

--- environment/include/math/math.h
+++ environment/include/math/math.h
@@ -80,7 +80,7 @@ class Vector : ISerializable{
 
     void clamp(T min, T max){
         for(int i = 0; i < L; i++){
-            vals[i] = std::min(std::max(vals[i],max),min);
+            vals[i] = std::max(std::min(vals[i],max),min);
         }
     }
     
